7d71a1a9e4ca6059b040d63d229eb22c7cbc071d
rahman <rahman.mahmoodi@gmail.com>
2021-01-02 17:19:57 +1100

    init

---
+++ BlazorTodos/Shared/ViewModels/TodosSummaryViewModel.cs
@@ -0,0 +1,21 @@
+﻿using BlazorTodos.Server.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorTodos.Shared.ViewModels
+{
+    public class TodosSummaryViewModel
+    {
+        private readonly List<TodoViewModel> _todos;
+
+        public TodosSummaryViewModel(List<TodoViewModel> todos)
+        {
+            _todos = todos;
+        }
+
+        public int TotalCompleted { get; set; } = 0;
+
+        public int TotalNotCompleted { get; set; } = 0;
+    }
+}
